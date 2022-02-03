2bf0c583b821b44eaaa9a8241604c3d18af9e19f
Effie330 <58240676+Effie330@users.noreply.github.com>
2022-02-02 23:21:07 -0600

    gradient, color changes, cleanup un-used code,

--- main.js
+++ main.js
@@ -7,7 +7,6 @@ app.whenReady().then(() => {
 })
   
 const createWindow = () =>{
-  export var selectedPage = "timerTab";
 
   //Create window
   const myWindow = new BrowserWindow({
@@ -18,19 +17,9 @@ const createWindow = () =>{
     }
   });
 
-  //load a webpage
-    
-  if(selectedPage == "graphTab"){
-    myWindow.loadFile('graphTab.html');
-  }else if(selectedPage == "timerTab"){
-    myWindow.loadFile('timerTab.html');
-  } else if(selectedPage == "solveTimesTab"){
-    myWindow.loadFile('solveTimesTab.html');
-  }
-  
-  console.log(selectedPage);
+  //Loading graph tab by default
+  myWindow.loadFile('graphTab.html');
 }
-export default selectedPage;
 
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') app.quit()
